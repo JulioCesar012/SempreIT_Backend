dc3abe7dcf22b68ba1f85a8ff21cede956684a4d
Julio Filho <rodriguesjuliocesar145@gmail.com>
2021-01-03 21:40:27 -0300

    Update serverInitialize.js

--- src/serverInitialize.js
+++ src/serverInitialize.js
@@ -11,7 +11,23 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.use(cors());
+app.use(cors({
+    origin: 'https://frontendsempreit.herokuapp.com'
+}));
+
+app.use((request, response, next) => {
+  response.header('Access-Control-Allow-Origin', 'https://frontendsempreit.herokuapp.com');
+  response.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  );
+
+  if(request.method === 'OPTIONS') {
+    response.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+    return response.status(200).send({});
+  }
+  next();
+});
 
 app.use(routes);
 
