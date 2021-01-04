e50a5f97cfbae1e2168bb00b1426c673f8286520
Julio Filho <rodriguesjuliocesar145@gmail.com>
2021-01-03 22:04:09 -0300

    Update connectionDBMysql.js

--- src/database/connectionDBMysql.js
+++ src/database/connectionDBMysql.js
@@ -2,9 +2,9 @@ const mysql = require('mysql');
 
 const pool = mysql.createPool({
   "host": "localhost",
-  "user": "root",
-  "password": "",
-  "database": "sempreitfullstack",
+  "user": "juliofilho",
+  "password": "%OR$C4zvM*TOL2ZD",
+  "database": "sempre_it_fullstack",
 });
 
 exports.pool = pool;
