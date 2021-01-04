ddf864bfa35c1dd0ebc5a54ca8e989705a8a082c
Julio Filho <rodriguesjuliocesar145@gmail.com>
2021-01-03 23:19:59 -0300

    Update connectionDB.js

--- src/database/connectionDB.js
+++ src/database/connectionDB.js
@@ -1,6 +1,6 @@
 const knex = require('knex');
 const configuration = require('../../knexfile');
 
-const connectionDB = knex(configuration.development);
+const connectionDB = knex(configuration.production);
 
 module.exports = connectionDB;
